d65a94f85937c59c01025d3bb72139f4fc4a2849
Charles Peden <ccbpeden@warpmail.net>
2017-01-13 13:11:12 -0800

    add branching logic to determine displayed information based on quiz

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,10 @@ var s2Output= function(username){
 
 var tendency;
 
+var count = function(string,char) {
+  return string.match(/char/g).length;
+};
+
 // Front End
 $(document).ready(function(){
   $("form#section1").submit(function(event){
@@ -38,38 +42,43 @@ $(document).ready(function(){
   $("form#section3").submit(function(event){
     event.preventDefault();
     var radio1 = ($("input:radio[name=clothes]:checked").val());
-    alert("radio1 set to" + radio1);
     tendency = radio1;
-    alert("tendency set to" + tendency);
     $("#section3").hide();
-    $(".s3Outputa").text(username);    
+    $(".s3Outputa").text(username);
     $("#section4").show();
   });
   $("form#section4").submit(function(event){
     event.preventDefault();
     var radio2 = ($("input:radio[name=location]:checked").val());
-    alert("radio2 set to" + radio2);
     tendency += radio2;
-    alert("tendency set to" + tendency);
     $("#section4").hide();
     $("#section5").show();
   });
   $("form#section5").submit(function(event){
     event.preventDefault();
     var radio3 = ($("input:radio[name=bus]:checked").val());
-    alert("radio3 set to" + radio3);
     tendency += radio3;
-    alert("tendency set to" + tendency);
     $("#section5").hide();
     $("#section6").show();
   });
   $("form#section6").submit(function(event){
     event.preventDefault();
     var radio4 = ($("input:radio[name=house]:checked").val());
-    alert("radio4 set to" + radio4);
     tendency += radio4;
-    alert("tendency set to" + tendency);
     $("#section6").hide();
     $("#conclusion").show();
+    alert(tendency);
+    var aVar = (tendency.match(/A/g));
+    var bVar = (tendency.match(/B/g));
+    var cVar = (tendency.match(/C/g));
+    var dVar = (tendency.match(/D/g));
+    if (aVar != null){
+      $("#php").show()};
+    if (bVar != null){
+      $("#java").show()};
+    if (cVar != null){
+      $("#cSharp").show()};
+    if (dVar != null){
+      $("#ruby").show()};
   });
 });
